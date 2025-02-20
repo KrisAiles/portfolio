efe9e151310c06f0b97793bf99a041250ca102bb
Kris <kris28@btinternet.com>
2025-02-20 22:45:26 +0000

    Added content to html files, added css styles for the updated html files, altered javascript animation and added form submit actions

--- resources/javascript/index.js
+++ resources/javascript/index.js
@@ -1,9 +1,30 @@
 // Assign id's to variables
+const bodyId = document.getElementsByTagName("body")[0];
 const backgroundContainer = document.getElementById("background-container");
 const mainContent = document.getElementById("page-container");
 const mainContentHeader = document.getElementById("page-container-header");
 const serverOffline = document.getElementById("server-offline");
 const serverStarted = document.getElementById("server-started");
+const formInput = document.forms["contact-form"];
+const formInputId = document.getElementById("contact-form");
+const formOutputContainer = document.getElementById("form-output");
+const formOutputContent = document.getElementById("form-output-content");
+const clearOutput = document.getElementById("reset");
+
+// Identify what page is active
+let loadAnimation;
+
+if (bodyId.id === "load-animation") {
+  loadAnimation = true;
+} else {
+  loadAnimation = false;
+}
+console.log(loadAnimation);
+
+// Display main content if no animation set
+if (!loadAnimation) {
+  mainContent.style.transform = "none";
+}
 
 // Get screen size details
 const screenWidth = window.innerWidth;
@@ -30,23 +51,57 @@ if(screenWidth <= mobileWidth) {
 };
 
 // This is the start of the page load animation sequence
-setTimeout(function () {
-  serverOffline.style.display = "block"; 
-}, 2000);
+function pageLoadAnimation() { 
+  setTimeout(function () {
+    serverOffline.style.display = "block"; 
+    setTimeout(function () {
+      serverOffline.style.display = "none";
+      serverStarted.style.display = "block";
+      setTimeout(function () {
+        serverStarted.style.display = "none";
+        mainContent.style.animation = "slide 1s forwards";
+      }, 2000);
+    }, 2000);
+  }, 2000);
+};
 
-setTimeout(function () {
-  serverStarted.style.display = "block";
-}, 4000); 
 
-setTimeout(function () {
-  serverOffline.style.display = "none";
-}, 4000);
-
-setTimeout(function () {
-  serverStarted.style.display = "none";
-}, 6000);
-
-setTimeout(function() {
-  mainContent.style.animation = "slide 1s forwards";
-}, 6000);
+if (loadAnimation) {
+  pageLoadAnimation();
+};
 // This is the end of the page load animation sequence
+
+// Display form data
+formInput.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  formInputId.style.display = "none";
+  formOutputContainer.style.display = "flex";
+
+  const formData = {
+    name: this.name.value,
+    email: this.email.value,
+    message: this.message.value
+  }
+
+  const formOutput = `
+    <p>Name: ${formData.name}<br><br>
+    Email: ${formData.email}<br><br>
+    Message: ${formData.message}</p>
+  `;
+
+  formOutputContent.innerHTML = formOutput;
+
+  this.name.value = "";
+  this.email.value = "";
+  this.message.value = "";
+});
+
+clearOutput.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  formInputId.style.display = "flex";
+  formOutputContainer.style.display = "none";
+  formOutputContent.innerHTML = "";
+});
+// This is the end of display form data
